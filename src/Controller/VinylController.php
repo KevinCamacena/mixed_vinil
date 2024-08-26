3adc320b33084faccad63c88fa96e7d8eecd1274
Kevin Camacena <kevincamacenabenoit@gmail.com>
2024-08-26 17:08:40 -0600

    Adding homepage twig template

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,16 +2,31 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
 
-class VinylController
+class VinylController extends AbstractController
 {
     #[Route('/')]
     public function homepage(): Response
     {
-        return new Response('Welcome to the Vinyl Shop!');
+        $tracks = [
+            ['song' => "Gansgta's Paradise", 'artist' => 'Coolio'],
+            ['song' => "Smells Like Teen Spirit", 'artist' => 'Nirvana'],
+            ['song' => "The Sign", 'artist' => 'Ace of Base'],
+            ['song' => "No Diggity", 'artist' => 'Blackstreet'],
+            ['song' => "I Want It That Way", 'artist' => 'Back'],
+            ['song' => "Say My Name", 'artist' => "Destiny's Child"],
+            ['song' => "I Will Always Love You", 'artist' => 'Whitney Houston'],
+
+        ];
+
+        return $this->render('vinyl/homepage.html.twig',[
+            'title' => 'PB & Jams',
+            'tracks' => $tracks,
+        ]);
     }
 
     #[Route('/browse/{slug}')]
@@ -19,8 +34,10 @@ class VinylController
     {
         if ($slug) {
             $title = "Genre:".u(str_replace('-', ' ', $slug))->title(true);
+        } else {
+            $title = "Browse Vinyl";
         }
-        $title = 'All Genres';
+
         return new Response($title);
     }
 }
