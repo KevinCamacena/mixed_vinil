5b4f5d0c9747691d610ec9219b13aee6de846388
Kevin Camacena <kevincamacenabenoit@gmail.com>
2024-09-06 23:01:30 -0600

    Adding more custom queries to MixRepository

--- src/Controller/MixController.php
+++ src/Controller/MixController.php
@@ -13,16 +13,19 @@ class MixController extends AbstractController
     #[Route('/mix/new', name: 'mix_new')]
     public function new(EntityManagerInterface $entityManager): Response
     {
+        $genres = ['Urban', 'Rock', 'Pop', 'Dance', 'R&B', 'Country', 'Jazz', 'Classical', 'Blues', 'Reggae', 'Metal',
+            'Electronic', 'Folk', 'Indie', 'Punk', 'Soul', 'Gospel', 'Latin', 'World', 'New Age', 'Soundtrack', 'Vocal',
+            'Easy Listening', 'Comedy', 'Children', 'Holiday', 'Opera', 'Avant-Garde', 'Miscellaneous'];
         $mix = new VinylMix();
         $mix->setTitle('Old Urban Mix');
-        $mix->setDescription('A mix of old school Urban tracks');
-        $mix->setGenre('Urban');
+        $mix->setDescription(sprintf('A mix of old school %s tracks', $mix->getGenre()));
+        $mix->setGenre($genres[array_rand($genres)]);
         $mix->setTrackCount(rand(4,20));
         $mix->setVotes(rand(-50, 50));
 
         $entityManager->persist($mix);
         $entityManager->flush();
 
-        return new Response(sprintf('New Mix %d is %d tracks of pure %d', $mix->getId(), $mix->getTrackCount(), $mix->getGenre()));
+        return new Response(sprintf('New Mix %d of %s is %d tracks of pure %s', $mix->getId(), $mix->getGenre() ,$mix->getTrackCount(), $mix->getGenre()));
     }
 }
